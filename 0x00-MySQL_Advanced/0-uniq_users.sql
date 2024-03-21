8f755e644606432401625ac0ffa9b743eacd1129
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-03-21 19:16:23 +0000

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,9 @@
+-- This script creates a table named 'users' in a MySQL database, ensuring it only 
+-- occurs if the table doesn't already exist with the following requirements:
+--  - id: Integer, not null, auto-incrementing primary key
+--  - email: String (255 characters), not null, unique
+--  - name: String (255 characters)
+
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
