28af1dd722d8e2c751baf197c99032afa1bf4988
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-03-21 20:54:28 +0000

    Update 11-need_meeting.sql

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -3,7 +3,7 @@
 -- or more than 1 month.
 
 DROP VIEW IF EXISTS need_meeting;
-CREATE VIEW need_meeting AS mine
+CREATE VIEW need_meeting AS
        SELECT name
        FROM students
        WHERE students.score < 80 AND
