c71a2758bd308b45b58af6fecf025d27d375a03b
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-03-26 06:10:13 +0000

    python script to list all docs in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""interpreter used"""
+
+
+def list_all(mongo_collection):
+  """
+  Python function that lists all documents in a collection
+  Returns:
+  lists all documents in mongo_collection
+  """
+  documents = mongo_collection.find()
+  document_list = list(documents)
+  return document_list
