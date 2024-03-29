77519d4b5d3d7eac99f51d24e16afeb701887fa1
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-03-29 11:44:57 +0000

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """interpreter used"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -18,3 +18,22 @@ class Cache:
         r_key = str(uuid.uuid4())
         self._redis.set(r_key, data)
         return r_key
+
+    def get(self, key: str, fn: Callable = None) ->
+    Union[str, bytes, int, None]:
+        """method that take a key string argument
+        and an optional Callable argument named fn"""
+        new_data = self._redis.get(key)
+        if new_data is None:
+            return None
+        if fn is not None:
+            return fn(new_data)
+        return new_data
+
+    def get_str(self, key: str) -> Union[str, bytes, None]:
+        """function that retrieves string data from cache"""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """function that retrieve integer data from cache"""
+        return self.get(key, fn=int)
